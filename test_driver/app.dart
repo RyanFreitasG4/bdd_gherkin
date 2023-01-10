8c07a25a543676dadb0197e83bb92c7b0e930f07
RyanFreitasG4 <ryan@g4tech.com.br>
2023-01-10 20:44:55 -0300

    adicionado diretorios e arquivos de configuração

---
+++ test_driver/app.dart
@@ -0,0 +1,8 @@
+import 'package:bdd_gherkin/main.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+  runApp(const MyApp());
+}
